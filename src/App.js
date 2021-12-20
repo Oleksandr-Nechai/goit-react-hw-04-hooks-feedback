aa6a6afda120a96bcf172d8928639f6204dfc50e
Oleksandr-N <hekp1984@gmail.com>
2021-12-20 10:06:06 +0200

    ffdfd

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import { useState } from "react";
-import Statistics from "./components/Statistics";
-import FeedbackOptions from "./components/FeedbackOptions";
-import Notification from "./components/Notification";
-import Section from "./components/Section";
-import "./App.css";
+import { useState } from 'react';
+import Statistics from './components/Statistics';
+import FeedbackOptions from './components/FeedbackOptions';
+import Notification from './components/Notification';
+import Section from './components/Section';
+import './App.css';
+
+const initialState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [state, setState] = useState(initialState);
 
-  const buttonsOption = ["good", "neutral", "bad"];
-
-  const onClickButton = (option) => {
+  const onClickButton = option => {
     switch (option) {
       case buttonsOption[0]:
-        setGood((state) => state + 1);
+        setGood(state => state + 1);
         break;
       case buttonsOption[1]:
-        setNeutral((state) => state + 1);
+        setNeutral(state => state + 1);
         break;
       case buttonsOption[2]:
-        setBad((state) => state + 1);
+        setBad(state => state + 1);
         break;
       default:
         return;
@@ -39,10 +41,7 @@ const App = () => {
   return (
     <>
       <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={buttonsOption}
-          onLeaveFeedback={onClickButton}
-        ></FeedbackOptions>
+        <FeedbackOptions options={buttonsOption} onLeaveFeedback={onClickButton}></FeedbackOptions>
       </Section>
       <Section title="Statistic">
         {countTotalFeedback() ? (
